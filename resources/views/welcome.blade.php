f5d45717a188b761674e5a61b4c909fb34e39060
thanaoei <oeiandta@gmail.com>
2020-03-13 12:03:06 +0700

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,27 +32,13 @@
     </div>
     <div class="col-xs-12 text-center">
         <div class="col-xs-4" style="padding:30px;" v-for="property in properties" is="property" :property="property" >
-
+            {{-- <div class="colx-xs-12" is="sortColumn":property="buttonSort" >test</div> --}}
         </div>
+
     </div>
-    <template id="template-property">
-        <div class="card text-left">
-            <div class="card-title text-center"><b>@{{property.title}}</b></div>
-            <div class="card-text"><b>Project Name :</b>@{{property.project_name}}</div>
-            <div class="card-text"><b>Property type: </b> @{{property.property_type}}</div>
-            <div class="card-text"><b>Description: </b> @{{property.description}}</div>
-            <div class="card-text"><b>Country: </b> @{{property.country}}</div>
 
-            <div class="card-text"><b>Status: </b> @{{property.status}}</div>
-            <div class="card-text"><b>For Sale: </b> @{{property.for_sale}}</div>
-            <div class="card-text"><b>For Rent: </b> @{{property.for_rent}}</div>
-            <div class="card-text"><b>Bedroom: </b> @{{property.bedroom}}</div>
-            <div class="card-text"><b>Bathroom: </b> @{{property.bathroom}}</div>
-        </div>
-    </template>
     <div class="col-xs-12">
         <div class='row'>
-
             <div class="text-right col-xs-12 pagination text-center">
                 <button class="btn btn-default" @click="fetchproperties(pagination.prev_page_url)"
                         :disabled="!pagination.prev_page_url">
@@ -64,14 +50,42 @@
                 </button>
             </div>
         </div>
+    </div>
 </div>
+
+<template id="template-property">
+    <div class="card text-left">
+        <div class="card-title text-center"><b>@{{property.title}}</b></div>
+        <div class="card-text"><b>Project Name :</b>@{{property.project_name}}</div>
+        <div class="card-text"><b>Property type: </b> @{{property.property_type}}</div>
+        <div class="card-text"><b>Description: </b> @{{property.description}}</div>
+        <div class="card-text"><b>Country: </b> @{{property.country}}</div>
+        <div class="card-text"><b>Status: </b> @{{property.status}}</div>
+        <div class="card-text"><b>For Sale: </b> @{{property.for_sale}}</div>
+        <div class="card-text"><b>For Rent: </b> @{{property.for_rent}}</div>
+        <div class="card-text"><b>Bedroom: </b> @{{property.bedroom}}</div>
+        <div class="card-text"><b>Bathroom: </b> @{{property.bathroom}}</div>
+    </div>
+</template>
+<template id="buttonSort">
+    <button class="btn btn-default text-center" @click="changeSort">
+        Sort by: @{{ property.sort }}
+    </button>
+</template>
 
 <script src="http://cdnjs.cloudflare.com/ajax/libs/vue/1.0.24/vue.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vue-resource/0.7.0/vue-resource.js"></script>
 <script type="text/javascript">
+
+    let test2 = Vue.component('sortColumn', {
+        template: '#buttonSort',
+        props:['buttonSort']
+    })
+
     Vue.component('property', {
         template: '#template-property',
         props: ['property'],
+        // 'componentSort': test2
     })
 
     new Vue({
@@ -88,12 +102,13 @@
         methods: {
             fetchproperties: function (page_url) {
                 let vm = this;
-                console.log('vm:',vm)
                 page_url = page_url || '/api/fazwaz/properties'
                 this.$http.get(page_url)
                     .then(function (response) {
                         vm.makePagination(response.data.pagination)
                         vm.$set('properties', response.data.data)
+                        vm.changeSort(response.data.data)
+
                     });
             },
             makePagination: function(data){
@@ -105,7 +120,11 @@
                 }
                 // console.log(data.current_page)
                 this.$set('pagination', pagination)
+            },
+            changeSort: function(data){
+                console.log(data)
             }
+
         }
     });
 </script>
